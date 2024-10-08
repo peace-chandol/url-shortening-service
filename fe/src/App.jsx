92fa423e32159fda1c5267e2a30489f6364dcaea
Peace Chandol <peace.hyperion@gmail.com>
2024-10-09 02:50:35 +0700

    table relationship

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
         <RedirectComponent />
         <GenerateShortUrlComponent />
-        <TableUrlComponent />        
+        <TableUrlComponent />
         
     </>
   )
--- fe/src/components/TableUrlComponent.jsx
+++ fe/src/components/TableUrlComponent.jsx
@@ -1,17 +1,48 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+
 const TableUrlComponent = () => {
+    const [allURLs, setAllURLs] = useState([])
+
+
+    useEffect(() => {
+        getURLs()
+    },[])
+
+    async function getURLs() {
+        try {
+            const url = 'http://localhost:3000/shortening'
+            const response = await axios.get(url)
+            const data = response.data
+            setAllURLs(data)
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
     return (
         <div>
             <h2>Table Show Relationship between Long Url and Short Url</h2>
             <div>
                 <table>
-                    <tr>
-                        <th>Long Url</th>
-                        <th>Short Url</th>
-                    </tr>
-                    <tr>
-                        <td>Example of Long Url</td>
-                        <td>Example of Short  Url</td>
-                    </tr>
+                    <thead>
+                        <tr>
+                            <th>Long Url</th>
+                            <th>Short Url</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* <tr>
+                            <td>Example of Long Url</td>
+                            <td>Example of Short  Url</td>
+                        </tr> */}
+                        {allURLs.map((url) => (
+                            <tr key={url.id}>
+                                <td>{url.longUrl}</td>
+                                <td>{url.shortUrl}</td>
+                            </tr>
+                        ))}
+                    </tbody>
                 </table>
             </div>
         </div>
