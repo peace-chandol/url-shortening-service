3700c6e151b26c5a32a5d343cd2c7e28bd037d7c
Peace Chandol <peace.hyperion@gmail.com>
2024-10-09 03:24:59 +0700

    generate short Url

--- fe/src/components/GenerateShortUrlComponent.jsx
+++ fe/src/components/GenerateShortUrlComponent.jsx
@@ -1,11 +1,47 @@
+import { useState } from "react"
+import axios from "axios"
+
 const GenerateShortUrlComponent = () => {
+    const [longUrl, setLongUrl] = useState('')
+
+    async function handleCreateShortUrl() {
+        try {
+            const url = 'http://localhost:3000/shortening'
+            const data = { longUrl }
+            const response = await axios.post(url, data, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            clearForm()
+            window.location.reload()
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+    function handleCreateShortUrlBtn() {
+        if (longUrl) {
+            handleCreateShortUrl()
+        }
+        else console.log('empty input')
+    }
+
+    function clearForm() {
+        setLongUrl('')
+    }
+
+    function handleInputLongUrl(e) {
+        setLongUrl(e.target.value)
+    }
+
     return (
         <div>
             <h2>Generate Short URL</h2>
             <h3>Enter Long URL</h3>
             <div>
-                <input type="text" />
-                <button>Create</button>
+                <input type="text" id='generateShortUrl' value={longUrl} onChange={handleInputLongUrl}/>
+                <button id='createShortUrlBtn' onClick={handleCreateShortUrlBtn}>Create</button>
             </div>
         </div>
     )
