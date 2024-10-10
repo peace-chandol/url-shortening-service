8e94bce847a14752cfb1d6a837561fc7fade09a0
Peace Chandol <peace.hyperion@gmail.com>
2024-10-11 03:02:03 +0700

    get longUrl from shortUrl

--- fe/src/components/RedirectComponent.jsx
+++ fe/src/components/RedirectComponent.jsx
@@ -1,12 +1,45 @@
+import { useState } from "react"
+import axios from "axios"
+
+
 const RedirectComponent = () => {
+    const [shortUrl, setShortUrl] = useState('')
+    const [errorText, setErrorText] = useState(null)
+    const [result, setResult] = useState('')
+
+    function handleRedirectLongUrlBtn(e) {
+        setShortUrl(e.target.value)
+    }
+
+    async function handleGetLongUrl() {
+        if (!shortUrl) return setErrorText('Please Fill Short URL')
+        try {
+            const url = `http://localhost:3000/shortening/${shortUrl}`
+            const response = await axios.get(url)
+            const data = response.data
+            setResult(data.longUrl)
+            clearForm()
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+    function clearForm() {
+        setShortUrl('')
+        setErrorText('')
+    }
+
+
     return (
         <div>
             <h2>Redirect</h2>
             <h3>Enter short URL</h3>
+            <div>{ errorText }</div>
             <div>
-                <input type="text" />
-                <button>Find</button>
+                <input type="text" onChange={handleRedirectLongUrlBtn} value={shortUrl} />
+                <button onClick={handleGetLongUrl}>Find</button>
             </div>
+            <div>{ result }</div>
         </div>
     )
 }
